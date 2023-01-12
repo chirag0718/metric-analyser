59452b90a420ff6b63b24692bcd7ae7bd8b6a0d5
Chiragkumar Patel <chirag.patel0718@gmail.com>
2023-01-12 14:12:42 +0000

    Add Matric Analyser route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MetricAnalyserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/metric-analyser', [MetricAnalyserController::class, 'processData']);
